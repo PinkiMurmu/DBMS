27422bf8c70d6ed09bce201e499f77ece423c986
Pinki Murmu <149937432+PinkiMurmu@users.noreply.github.com>
2024-11-08 23:08:21 +0530

    Update First.sql

--- First.sql
+++ First.sql
@@ -47,4 +47,3 @@ select * from Staff where salary > 10000;
 select * from Staff where salary between 20000 and 30000;
 
 select * from Staff where position = 'Manager' and position = 'Supervisor';
-
